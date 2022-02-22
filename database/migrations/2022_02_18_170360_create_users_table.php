38c643f1488b51e6e754efaa3f558b6f57c3ad2f
unknown <haniemcs@gmail.com>
2022-02-22 15:41:07 +0500

    Users table edit

--- database/migrations/2022_02_18_170360_create_users_table.php
+++ database/migrations/2022_02_18_170360_create_users_table.php
@@ -16,6 +16,10 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('surname');
+            $table->string('lastname');
+            $table->string('role');
+            $table->foreignId('id_teacher')->references('id')->on('teachers')->nullable();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
